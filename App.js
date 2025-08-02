d9b5d0abb891e2d8145d1bd40236031ada8e471f
Nishant Raghav <nishantxraghav@gmail.com>
2025-08-02 20:58:40 +0530

    adding our project

---
+++ App.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";    
+const header = React.createElement("h1", {}, "This is namaste react🚀");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(header);
